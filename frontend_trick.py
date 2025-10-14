bd052ad815c15baa7287804912e953f1a2b2d442
macul99 <23519286+macul99@users.noreply.github.com>
2025-10-14 14:55:03 +0800

    Create frontend_trick.py

---
+++ frontend_trick.py
@@ -0,0 +1,13 @@
+# how to center an item in a webpage
+# 1. display: block;
+# 2. set width of the item
+# 3. set margin-left and margin-right to auto
+
+# margin controls the space outside an element
+# padding controls the space inside an element
+# boarder controls the space between margin and padding
+# The box model: margin -> boarder -> padding -> content
+
+# flex box
+# display: flex
+# justify-content: center/end/space-around
