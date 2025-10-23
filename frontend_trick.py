bd96bdc5f29d846f03bd75255281df405614ffdf
macul99 <23519286+macul99@users.noreply.github.com>
2025-10-23 09:31:26 +0800

    Enhance frontend_trick.py with flexbox and font notes
    Added comments and examples for flexbox and font usage.

--- frontend_trick.py
+++ frontend_trick.py
@@ -8,9 +8,35 @@
 # boarder controls the space between margin and padding
 # The box model: margin -> boarder -> padding -> content
 
-# flex box
-# display: flex
-# justify-content: center/end/space-around
+## flex box
+# display: flex;
+# flex-direction: row; column
+# justify-content: start; center, end, space-around, space-between # main-axis (horizontal direction for row direction)
+# align-items: start; center, end, stretch (default) # cross-axis (vertical direction for row direction)
 
 # coolors.co # color and hex code
+
+# userway.org # color contrast check
+
+# unsplash.com # high quality image for website
+
+# animation picture
+# use .webp format, smaller size than GIFs
+# giphy.com offer good images
+
+# new font class WAF2 is smaller and better
+# fonts.google.com # customized web fonts
+# 1001fonts.com # all special font, add .ttf file downloaded
+@font-face {
+  src: url("Corleone.ttf");
+  font-family: Corleone;
+}
+
+h1 {
+  font-family: Corleone;
+}
+
+# <span> inline version of <div>
+
+
 
