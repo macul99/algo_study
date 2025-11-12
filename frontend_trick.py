751bbf8681c6f33cde8ec8e931ab3f5fe65140b5
macul99 <23519286+macul99@users.noreply.github.com>
2025-11-12 15:54:37 +0800

    Document CSS margin behavior and specificity rules
    Added comments explaining CSS behavior and specificity.

--- frontend_trick.py
+++ frontend_trick.py
@@ -39,4 +39,35 @@ h1 {
 # <span> inline version of <div>
 
 
+# Strange Margin Behavior
+# - When an element touches its parent, its top and bottom margins will merge with the margins of the parent element
+# - What's the solution? Ans: add padding to parent element
+# - This is NOT an issue when u are using flexbox and grid
+
+# Pseudo Selector: .visited .hover .focus .active ## Note: order matters, hover/focus will overide active
+a:hover, a:focus {
+}
+
+# Specificity
+# - For same specificity selector, item of btm wins
+# - Order of specificity
+# - order from low to high: element (1 point), class (10 points), id (100 points)
+
+# Compound selectors: seperated by ' '. parent_selector child_selector
+# - specificity of compound is the sum of specificity of parent and child selectors
+# - not encouraged to use compound selector due to the troublesome of cal the specificity
+.ad-container a{ # tag a inside .ad-container
+    color: limegreen;
+    font-size: 18px;
+}
+
+# !importand keyword - to override other selectors
+# - not encouraged to use
+p{
+    color: red !important;
+}
+
+
+
+
 
